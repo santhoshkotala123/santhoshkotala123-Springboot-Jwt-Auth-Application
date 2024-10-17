b88bcde834e4c11d88c774b6ac51078b552cc769
Kotala Santhosh <143922100+santhoshkotala123@users.noreply.github.com>
2024-10-17 10:05:33 +0530

    the initial files

---
+++ src/main/java/com/tericcabrel/authapi/SpringbootJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtAuthApplication.class, args);
+	}
+
+}
